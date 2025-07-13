ca13be877aa705539ecafb8ebec5cab60aa93f11
Paolo Di Giglio <p.digiglio91@gmail.com>
2025-07-13 11:27:05 +0200

    Re-format stat_assert.h

--- stat_assert.h
+++ stat_assert.h
@@ -1,15 +1,15 @@
 #pragma once
 
 /**
-* \brief Replicate the behavior of `static_assert`. The idea is to have a class
-* template that is only complete in the `true` case (see below).
-*/
+ * \brief Replicate the behavior of `static_assert`. The idea is to have a class
+ * template that is only complete in the `true` case (see below).
+ */
 template <bool> //
 struct stat_assert;
 
 /**
-* \brief Specialize the `true` case to be completed.
-*/
+ * \brief Specialize the `true` case to be completed.
+ */
 template <> //
 struct stat_assert<true> {
   /**
