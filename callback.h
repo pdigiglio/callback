61c1726f671f02a99c4f21ebaeb4a7f159647b1f
Paolo Di Giglio <p.digiglio91@gmail.com>
2025-07-15 23:04:58 +0200

    callback.h: introduce base class
    The way I implemented Callback<>, there's no reason to specialize it
    twice depenting on whether the callback signature is void or not.

--- callback.h
+++ callback.h
@@ -2,115 +2,346 @@
 
 #include "stat_assert.h"
 
-#include <cstdio>
-#include <cstdlib>
+#include <cassert>
 #include <cstring>
-#include <memory>
-#include <bitset>
-#include <iostream>
 
-#define Ellipsis
+template <unsigned N, typename T>
+unsigned copy_on_buffer(T const &value, char (&buffer)[N]) {
+  static const unsigned valueSize = sizeof(value);
+  stat_assert<valueSize <= N>("Not enough memory in the buffer");
 
+  typedef char BufferForValue[valueSize];
+  BufferForValue const &valueBuffer = reinterpret_cast<BufferForValue const &>(value);
 
+  static const unsigned bytesToCopy = valueSize < N ? valueSize : N;
+  std::memcpy(buffer, valueBuffer, bytesToCopy);
+  return bytesToCopy;
+}
 
-
-template <typename> //
-class Callback;
-
-template <typename R, typename Ellipsis Args> //
-class Callback<R(Args Ellipsis)> {
-
-  /** \brief The type of the target signature. */
-  typedef R(SignatureT)(Args Ellipsis);
-
-  /** \brief The type of this Callback<> object. */
-  typedef Callback<SignatureT> SelfT;
-
-  /**
-   * \brief The type of a member function pointer with the same signature as
-   * SignatureT as a member of this class.
-   *
-   * I'll use this type to reserve a buffer to store a member function pointer
-   * address, assuming that _every_ member function pointer has the same size.
-   */
-  typedef SignatureT SelfT::*MyMemPtrType;
-
-  /**
-   * \brief The type of a pointer to a free (or static member) function with
-   * the same signature as SignatureT.
-   */
-  typedef SignatureT *FunctionPtrT;
-
-  /**
-   * \brief The type of the buffer for the member function pointer.
-   */
-  typedef char MemberPtrBufferT[sizeof(MyMemPtrType)];
-
-  template <typename RR, typename Ellipsis AArgs>
-  static R call_free_func(SelfT const *self, Args Ellipsis args) {
-    typedef RR(ActualFuncType)(AArgs Ellipsis);
-    return static_cast<ActualFuncType *>(self->_f)(args Ellipsis);
-  }
-
-  template <typename C, typename RR, typename U, typename Ellipsis AArgs>
-  static R call_mem_func(SelfT const *self, Args Ellipsis args) {
-    std::puts("mem");
-    C &callee = *static_cast<C *>(self->_callee);
-
-    typedef RR(ActualFuncType)(AArgs Ellipsis);
-    ActualFuncType U::*memFun =
-        reinterpret_cast<ActualFuncType U::*const &>(self->_mf);
-
-    return (callee.*memFun)(args Ellipsis);
-  }
-
-  template <typename C, typename RR, typename U, typename Ellipsis AArgs>
-  static R call_const_mem_func(SelfT const *self, Args Ellipsis args) {
-    C const &callee = *static_cast<C *>(self->_callee);
-
-    typedef RR(ActualFuncType)(AArgs Ellipsis) const;
-    ActualFuncType U::*memFun =
-        reinterpret_cast<ActualFuncType U::*const &>(self->_mf);
-
-    return (callee.*memFun)(args Ellipsis);
-  }
-
-public:
-  template <typename RR, typename Ellipsis AArgs>
-  explicit Callback(RR (*f)(AArgs Ellipsis)) //
-      : _thunk(call_free_func<RR, AArgs Ellipsis>), _f(f), _callee(0) {}
-
-  template <typename C, typename RR, typename U, typename Ellipsis AArgs>
-  explicit Callback(C &obj, RR (U::*mf)(AArgs Ellipsis)) //
-      : _thunk(call_mem_func<C, RR, U, AArgs Ellipsis>), _callee(&obj) {
-    static const int mf_size = sizeof(mf);
-    stat_assert<mf_size <= sizeof(_mf)>("Not enough memory in _mf");
-    std::memcpy(_mf, reinterpret_cast<char const(&)[mf_size]>(mf), mf_size);
-  }
-
-  // template <typename C, typename RR, typename U, typename Ellipsis AArgs>
-  // explicit Callback(C const &obj, RR (U::*mf)(AArgs Ellipsis) const) //
-  //     : _thunk(call_const_mem_func<C, RR, U, AArgs Ellipsis>), _callee(&obj)
-  //     {
-  //   stat_assert<sizeof(mf) <= sizeof(_mf)>("Not enough memory in _mf");
-  //   std::memcpy(_mf, (char const *)mf, sizeof(mf));
-  // }
-
-  R operator()(Args Ellipsis args) const { return _thunk(this, args Ellipsis); }
-
+/**
+ *
+ * Since I have no variadic template in C++98, I factor out as much as the
+ * common functionality of Callback in this base class. If I could expand a
+ * parameter pack, I could do all of this in Callback<>.
+ */
+class CallbackBase {
 private:
-  R (*_thunk)(SelfT const *, Args Ellipsis);
+  typedef void(DummySignature)();
+
+  /**
+   * \brief A function pointer type that I'll use as a reference.
+   * \remark I am assuming that all the function pointers have the same length!
+   */
+  // typedef void (*FuncPtr)();
+  typedef DummySignature *FuncPtr;
+  typedef char FuncPtrBuffer[sizeof(FuncPtr)];
+
+  /**
+   * \brief A membef function pointer type that I'll use as a reference.
+   *
+   * \remark I am assuming that all the member function pointers have the same
+   * length! This doesn't appear to be true on MSVC.
+   */
+  typedef DummySignature CallbackBase::*MemFuncPtr;
+  typedef char MemFuncPtrBuffer[sizeof(MemFuncPtr)];
+
+protected:
+  explicit CallbackBase() //
+      : _fAlign_(0), _callee(0) {}
+
+  template <typename FPtr>
+  explicit CallbackBase(FPtr *fptr) //
+      : _fAlign_(0), _callee(0) {
+    // Initialize the free-function pointer.
+    copy_on_buffer(fptr, _f);
+  }
+
+  template <typename C, typename U, typename FPtr>
+  explicit CallbackBase(C &callee, FPtr U::*memFptr) //
+      : _mfAlign_(0), _callee(static_cast<void *>(&callee)) {
+    // Initialize the mem-function pointer.
+    copy_on_buffer(memFptr, _mf);
+  }
+
+  bool isSet() const {
+    if (_callee) {
+      assert(_fAlign_);
+      return true;
+    }
+
+    return _fAlign_;
+  }
 
   union {
-    /** \brief The function pointer. */
-    FunctionPtrT _f;
-    /** \brief The member function pointer. */
-    MemberPtrBufferT _mf;
+    /** \brief The buffer for the function pointer. */
+    FuncPtrBuffer _f;
 
-    // For alignment
-    MemberPtrBufferT _;
+    /** \brief A function pointer insure the union alignment. */
+    FuncPtr _fAlign_;
+
+    /** \brief The buffer for the member function pointer. */
+    MemFuncPtrBuffer _mf;
+
+    /** \brief A member function pointer insure the union alignment. */
+    MemFuncPtr _mfAlign_ /* unused */;
   };
 
   /** \brief The target callee object (if any). */
   void *_callee;
 };
+
+template <typename> //
+class Callback;
+
+// template <typename> //
+// struct CallbackTraits;
+//
+// template <typename SignatureT> //
+// struct CallbackTraits<Callback<SignatureT> > {
+//   /** \brief The type of the target signature. */
+//   typedef SignatureT Signature;
+//
+//   /** \brief The type of The Callback<> object. */
+//   typedef Callback<Signature> Self;
+//
+//   /**
+//    * \brief The type of a member function pointer with the same signature as
+//    * SignatureT as a member of this class.
+//    *
+//    * I'll use this type to reserve a buffer to store a member function pointer
+//    * address, assuming that _every_ member function pointer has the same size.
+//    */
+//   typedef Signature Self::*MyMemPtrType;
+//
+//   /**
+//    * \brief The type of a pointer to a free (or static member) function with
+//    * the same signature as SignatureT.
+//    */
+//   typedef Signature *FunctionPtr;
+//
+//   /**
+//    * \brief The type of the buffer for the member function pointer.
+//    */
+//   typedef char MemberPtrBuffer[sizeof(MyMemPtrType)];
+// };
+
+template <typename> //
+struct Tag {
+  /**\brief Ctor to avoid the most vexing parse. */
+  Tag(int) {}
+};
+
+template <typename R, typename Arg0> //
+class Callback<R(Arg0)> : protected CallbackBase {
+
+  /** \brief The type of this Callback<> object. */
+  typedef Callback<R(Arg0)> Self;
+
+  template <typename CBReturnT, typename CalleeSignatureT>
+  static CBReturnT call_free_func_impl(Self const *self, Arg0 arg, //
+                                       Tag<CalleeSignatureT>, Tag<CBReturnT>) {
+    CalleeSignatureT *const callee =
+        reinterpret_cast<CalleeSignatureT *const &>(self->_f);
+    return callee(arg);
+  }
+
+  template <typename CalleeSignatureT>
+  static void call_free_func_impl(Self const *self, Arg0 arg, //
+                                  Tag<CalleeSignatureT>, Tag<void>) {
+    CalleeSignatureT *const callee =
+        reinterpret_cast<CalleeSignatureT *const &>(self->_f);
+    callee(arg);
+  }
+
+  template <typename CalleeSignatureT>
+  static R call_free_func(Self const *self, Arg0 arg) {
+    Tag<CalleeSignatureT> const calleeT(0);
+    Tag<R> const returnT(0);
+    return call_free_func_impl(self, arg, calleeT, returnT);
+  }
+
+  template <typename CalleeT, typename CBReturnT, typename CalleeMemFunT>
+  static CBReturnT call_mem_func_impl(Self const *self, Arg0 arg, //
+                                      Tag<CalleeT>,               //
+                                      Tag<CalleeMemFunT>,         //
+                                      Tag<CBReturnT>) {
+    CalleeMemFunT const calleeMemFun =
+        reinterpret_cast<CalleeMemFunT const &>(self->_mf);
+    CalleeT *const callee = reinterpret_cast<CalleeT *>(self->_callee);
+    return (callee->*calleeMemFun)(arg);
+  }
+
+  template <typename CalleeT, typename CalleeMemFunT>
+  static void call_mem_func_impl(Self const *self, Arg0 arg, //
+                                 Tag<CalleeT>,               //
+                                 Tag<CalleeMemFunT>,         //
+                                 Tag<void>) {
+    CalleeMemFunT const calleeMemFun =
+        reinterpret_cast<CalleeMemFunT const &>(self->_mf);
+    CalleeT *const callee = reinterpret_cast<CalleeT *>(self->_callee);
+    (callee->*calleeMemFun)(arg);
+  }
+
+  template <typename C, typename CalleeMemFunT>
+  static R call_mem_func(Self const *self, Arg0 arg) {
+    Tag<C> const calleeT(0);
+    Tag<CalleeMemFunT> const calleeMemFunT(0);
+    Tag<R> const returnT(0);
+    return call_mem_func_impl(self, arg, calleeT, calleeMemFunT, returnT);
+  }
+
+public:
+  explicit Callback() //
+      : CallbackBase(), _thunk(0) {}
+
+  template <typename RR, typename AArgs>
+  explicit Callback(RR (*f)(AArgs)) //
+      : CallbackBase(f), _thunk(call_free_func<RR(AArgs)>) {}
+
+  template <typename C, typename RR, typename U, typename AArgs>
+  explicit Callback(C &obj, RR (U::*mf)(AArgs)) //
+      : CallbackBase(obj, mf), _thunk(call_mem_func<C, RR (U::*)(AArgs)>) {}
+
+  template <typename C, typename RR, typename U, typename AArgs>
+  explicit Callback(C &obj, RR (U::*mf)(AArgs) const) //
+      : CallbackBase(obj, mf),
+        _thunk(call_mem_func<C const, RR (U::*)(AArgs) const>) {}
+
+  R operator()(Arg0 args) const { return _thunk(this, args); }
+  using CallbackBase::isSet;
+
+private:
+  R (*_thunk)(Self const *, Arg0);
+};
+
+// template <typename Arg0> //
+// class Callback<void(Arg0)> : protected CallbackBase {
+//
+//   /** \brief The type of this Callback<> object. */
+//   typedef Callback<void(Arg0)> Self;
+//
+//   template <typename RR, typename AArgs>
+//   static void call_free_func(Self const *self, Arg0 arg) {
+//     typedef RR(ActualFuncType)(AArgs);
+//     reinterpret_cast<ActualFuncType *const &>(self->_f)(arg);
+//   }
+//
+//   template <typename C, typename RR, typename U, typename AArgs>
+//   static void call_mem_func(Self const *self, Arg0 arg) {
+//     C &callee = *static_cast<C *>(self->_callee);
+//
+//     typedef RR(ActualFuncType)(AArgs);
+//     ActualFuncType U::*memFun =
+//         reinterpret_cast<ActualFuncType U::*const &>(self->_mf);
+//
+//     (callee.*memFun)(arg);
+//   }
+//
+//   template <typename C, typename RR, typename U, typename AArgs>
+//   static void call_const_mem_func(Self const *self, Arg0 args) {
+//     C const &callee = *static_cast<C const *>(self->_callee);
+//
+//     typedef void(ActualFuncType)(AArgs) const;
+//     ActualFuncType U::*memFun =
+//         reinterpret_cast<ActualFuncType U::*const &>(self->_mf);
+//
+//     (callee.*memFun)(args);
+//   }
+//
+// public:
+//   explicit Callback() //
+//       : CallbackBase(), _thunk(0) {}
+//
+//   template <typename RR, typename AArgs>
+//   explicit Callback(RR (*f)(AArgs)) //
+//       : CallbackBase(f), _thunk(call_free_func<RR, AArgs>) {}
+//
+//   template <typename C, typename RR, typename U, typename AArgs>
+//   explicit Callback(C &obj, RR (U::*mf)(AArgs)) //
+//       : CallbackBase(obj, mf), _thunk(call_mem_func<C, RR, U, AArgs>) {}
+//
+//   template <typename C, typename RR, typename U, typename AArgs>
+//   explicit Callback(C const &obj, RR (U::*mf)(AArgs)) //
+//       : CallbackBase(obj, mf), _thunk(call_const_mem_func<C, RR, U, AArgs>) {}
+//
+//   void operator()(Arg0 args) const { return _thunk(this, args); }
+//   using CallbackBase::isSet;
+//
+// private:
+//   void (*_thunk)(Self const *, Arg0);
+// };
+
+// template <typename R, typename Arg0> //
+// class Callback<R(Arg0)> {
+//
+//   /** \brief The type traits of this class */
+//   typedef CallbackTraits<Callback<R(Arg0)> > Traits;
+//
+//   template <typename RR, typename AArgs>
+//   static R call_free_func(typename Traits::Self const *self, Arg0 arg) {
+//     typedef RR(ActualFuncType)(AArgs);
+//     return static_cast<ActualFuncType *>(self->_f)(arg);
+//   }
+//
+//   template <typename C, typename RR, typename U, typename AArgs>
+//   static R call_mem_func(typename Traits::Self const *self, Arg0 arg) {
+//     C &callee = *static_cast<C *>(self->_callee);
+//
+//     typedef RR(ActualFuncType)(AArgs);
+//     ActualFuncType U::*memFun =
+//         reinterpret_cast<ActualFuncType U::*const &>(self->_mf);
+//
+//     return (callee.*memFun)(arg);
+//   }
+//
+//   template <typename C, typename RR, typename U, typename AArgs>
+//   static R call_const_mem_func(typename Traits::Self const *self, Arg0 args) {
+//     C const &callee = *static_cast<C *>(self->_callee);
+//
+//     typedef RR(ActualFuncType)(AArgs) const;
+//     ActualFuncType U::*memFun =
+//         reinterpret_cast<ActualFuncType U::*const &>(self->_mf);
+//
+//     return (callee.*memFun)(args);
+//   }
+//
+// public:
+//   template <typename RR, typename AArgs>
+//   explicit Callback(RR (*f)(AArgs)) //
+//       : _thunk(call_free_func<RR, AArgs>), _f(0), _callee(0) {
+//     copy_on_buffer(f, _f);
+//   }
+//
+//   template <typename C, typename RR, typename U, typename AArgs>
+//   explicit Callback(C &obj, RR (U::*mf)(AArgs)) //
+//       : _thunk(call_mem_func<C, RR, U, AArgs>), _callee(&obj) {
+//     copy_on_buffer(mf, _mf);
+//   }
+//
+//   // template <typename C, typename RR, typename U, typename  AArgs>
+//   // explicit Callback(C const &obj, RR (U::*mf)(AArgs ) const) //
+//   //     : _thunk(call_const_mem_func<C, RR, U, AArgs >), _callee(&obj)
+//   //     {
+//   //   stat_assert<sizeof(mf) <= sizeof(_mf)>("Not enough memory in _mf");
+//   //   std::memcpy(_mf, (char const *)mf, sizeof(mf));
+//   // }
+//
+//   R operator()(Arg0 args) const { return _thunk(this, args); }
+//
+// private:
+//   R (*_thunk)(typename Traits::Self const *, Arg0);
+//
+//   union {
+//     /** \brief The function pointer. */
+//     typename Traits::FunctionPtr _f;
+//
+//     /** \brief The member function pointer. */
+//     typename Traits::MemberPtrBuffer _mf;
+//
+//     // For alignment
+//     typename Traits::MemberPtrBuffer _;
+//   };
+//
+//   /** \brief The target callee object (if any). */
+//   void *_callee;
+// };
--- main.cpp
+++ main.cpp
@@ -2,7 +2,64 @@
 
 #include <cstdio>
 
+// Test:
+//  1. void can discard return value
+//  2. covariant return types
+//  3. contravariant param types
+
 namespace {
+
+class PrintScope {
+  char const *_scopeName;
+
+public:
+  PrintScope(char const *scopeName) //
+      : _scopeName(scopeName) {
+    std::printf("{ %s\n", _scopeName);
+  }
+
+  ~PrintScope() { //
+    std::printf("} %s\n", _scopeName);
+  }
+};
+
+#define PRINT_SCOPE PrintScope const _(__PRETTY_FUNCTION__) 
+
+struct Base {
+  virtual ~Base() {}
+
+  void f_void() {}
+  int f_int() { return 0; }
+
+  Base const *f_c(Base const &base) const {
+    PRINT_SCOPE;
+    return &base;
+  }
+
+  virtual Base const *f(Base const &base) {
+    PRINT_SCOPE;
+    return f_c(base);
+  }
+
+  // virtual int vf_int() { return 0; }
+};
+
+struct Derived : Base {
+  virtual Base const *f(Base const &derived) {
+    PRINT_SCOPE;
+    return &derived;
+  }
+
+  // virtual  int vf_int() { return 1; }
+};
+
+void foo(Base const &base) { //
+  std::printf("Base at %p\n", reinterpret_cast<void const *>(&base));
+}
+long bar(Derived const &derived) {
+  std::printf("Derived at %p\n", reinterpret_cast<void const *>(&derived));
+  return 0;
+}
 
 struct S {
   int i;
@@ -10,19 +67,48 @@ struct S {
 };
 
 void f(int i) { std::printf("i : %d\n", i); }
+
+
+// template <typename R,  >
+
 } // namespace
 
 int main() {
   {
-    Callback<void(int)> cb(f);
-    cb(0);
+    Derived d;
+    Callback<void(Derived const &)> cb;
+
+    {
+      cb = Callback<void(Derived const &)>(foo);
+      cb(d);
+    }
+
+    {
+      cb = Callback<void(Derived const &)>(bar);
+      cb(d);
+    }
+
+    {
+      cb = Callback<void(Derived const &)>(d, &Base::f);
+      cb(d);
+    }
+
+    {
+      cb = Callback<void(Derived const &)>(static_cast<Base &>(d), &Base::f_c);
+      cb(d);
+    }
   }
 
-  {
-    S s;
-    s.i = 1;
-
-    Callback<void(int)> cb(s, &S::f);
-    cb(0);
-  }
+  // {
+  //   Callback<void(int)> cb(f);
+  //   cb(0);
+  // }
+  //
+  // {
+  //   S s;
+  //   s.i = 1;
+  //
+  //   Callback<void(int)> cb(s, &S::f);
+  //   cb(0);
+  // }
 }
